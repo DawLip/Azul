f7bb662e4fbea9afc87c2d93916898f9564ae81f
Qler-gra <karasinski.kacper@outlook.com>
2020-03-27 11:38:38 +0100

    add TooManyPlayers

--- src/components/Game/TooManyPlayers/TooManyPlayers.jsx
+++ src/components/Game/TooManyPlayers/TooManyPlayers.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 
+import './tooManyPlayers.sass';
+
 class TooManyPlayers extends React.Component {
   render() {
-    return <div>Too many players</div>;
+    return (
+      <>
+        <div className="tooManyPlayers">
+          <div className="tooManyPlayers__top">
+            <i class="fas fa-user tooManyPlayers__icon"></i>
+            <i class="fas fa-user tooManyPlayers__icon"></i>
+            <p className="tooManyPlayers__text">Too many players</p>
+            <i class="fas fa-user tooManyPlayers__icon"></i>
+            <i class="fas fa-user tooManyPlayers__icon"></i>
+          </div>
+          <i class="fas fa-user-slash tooManyPlayers__bootomIcon"></i>
+        </div>
+      </>
+    );
   }
 }
 
