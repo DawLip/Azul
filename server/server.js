bbe8d0ba9d33940457a059399d7480b64fee741f
DawLip <lipax22@gmail.com>
2020-04-07 20:22:26 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
-import path from 'path'
-import express from 'express'
-import sockets from "./sockets/index.js"
+import path from 'path';
+import express from 'express';
+import sockets from './sockets/index.js';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
+console.log(DIST_DIR, HTML_FILE);
 
 const server = app.listen(port, () => console.log('App listening on port: ' + port));
 
 app.use(express.static(DIST_DIR));
 
-sockets(server)
+sockets(server);
 
 app.get('*', (req, res) => {
-	res.sendFile(HTML_FILE);
+  res.sendFile(HTML_FILE);
 });
